2533c45a46bec22758f784300490943d61cc3805
Ryan Kenning <rkenning2@yahoo.co.uk>
2020-06-16 22:03:56 +0100

    Update BigBen.py

--- BigBen.py
+++ BigBen.py
@@ -1,13 +1,21 @@
 def on_chat():
-    blocks.fill(BRICKS, pos(2, 0, 2), pos(4, 20, 4), FillOperation.REPLACE)
-    blocks.fill(BRICKS, pos(1, 20, 1), pos(5, 23, 5), FillOperation.REPLACE)
+    blocks.fill(BRICKS, pos(1, 0, 1), pos(5, 20, 5), FillOperation.REPLACE)
+    blocks.fill(BRICKS, pos(0, 20, 0), pos(6, 25, 6), FillOperation.REPLACE)
     blocks.fill(YELLOW_STAINED_GLASS_PANE,
-        pos(1, 20, 1),
-        pos(1, 23, 5),
+        pos(0, 20, 0),
+        pos(0, 25, 6),
+        FillOperation.REPLACE)
+    blocks.fill(BLACK_STAINED_GLASS_PANE,
+        pos(0, 22, 3),
+        pos(0, 24, 3),
+        FillOperation.REPLACE)
+    blocks.fill(BLACK_STAINED_GLASS_PANE,
+        pos(0, 22, 3),
+        pos(0, 22, 5),
         FillOperation.REPLACE)
     blocks.fill(OBSIDIAN,
-        pos(2, 24, 2),
-        pos(4, 25, 4),
+        pos(2, 26, 2),
+        pos(4, 29, 4),
         FillOperation.REPLACE)
     for index in range(5):
         pass
